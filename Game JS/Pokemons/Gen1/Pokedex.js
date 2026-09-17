ee84e5434636672da4ad03afe33d81d0f9642696
Davi Profeta <davihpg2008@gmail.com>
2026-09-17 11:17:01 -0300

    Add type and href to Pokemon entries in Pokedex

--- Game JS/Pokemons/Gen1/Pokedex.js
+++ Game JS/Pokemons/Gen1/Pokedex.js
@@ -1,4 +1,4 @@
 const pokedex = [
-    { nome: "Charmander", stats: Charmander, moves: movesFire.charmander},
-    { nome: "Gengar", stats: Gengar }
+    { nome: "Charmander", type: "fire", stats: Charmander, moves: movesFire.charmander, href: "https://img.pokemondb.net/sprites/black-white/anim/back-normal/charmander.gif"},
+    { nome: "Gengar", type: "ghost", stats: Gengar, moves: movesGhost.Gengar, href: "https://img.pokemondb.net/sprites/black-white/anim/back-normal/gengar.gif"}
 ];
