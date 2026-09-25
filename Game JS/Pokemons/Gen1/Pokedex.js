a0cdfabc4a6c31e5f727779826bc7cf45531eb90
Davi Profeta <davihpg2008@gmail.com>
2026-09-25 10:46:02 -0300

    Refactor Pokedex structure and image paths
    Updated Pokedex entries to include type2 and changed image paths.

--- Game JS/Pokemons/Gen1/Pokedex.js
+++ Game JS/Pokemons/Gen1/Pokedex.js
@@ -1,4 +1,4 @@
 const pokedex = [
-    { nome: "Charmander", type: "fire", stats: Charmander, moves: movesFire.charmander, hrefBack: "https://projectpokemon.org/images/sprites-models/normal-back/charmander.gif", hrefFront: "https://projectpokemon.org/images/normal-sprite/charmander.gif" },
-    { nome: "Gengar", type: "ghost", stats: Gengar, moves: movesGhost.Gengar, hrefBack: "https://projectpokemon.org/images/sprites-models/normal-back/gengar.gif", hrefFront: "https://projectpokemon.org/images/normal-sprite/gengar.gif"}
+    { nome: "Charmander", type1: "fire", type2: '', stats: Charmander, moves: movesFire.charmander, hrefBack: "/Game JS/Photos/charmander Back.gif", hrefFront: "/Game JS/Photos/charmander Front.gif" },
+    { nome: "Gengar", type1: "ghost", type2: 'poison', stats: Gengar, moves: movesGhost.Gengar, hrefBack: "/Game JS/Photos/gengar Back.gif", hrefFront: "/Game JS/Photos/gengar Front.gif"}
 ];
